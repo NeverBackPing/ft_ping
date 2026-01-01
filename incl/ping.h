b772eda1d5bdfca988ea730fef938cb491683988
NeverBackDown <neverbackping@gmail.com>
2026-01-01 05:52:31 +0100

    2026

--- incl/ping.h
+++ incl/ping.h
@@ -3,6 +3,18 @@
 
 #include "./dep/libft/libft.h"
 #include <stdio.h>
+#include <in.h> // sockaddr_in
+
+
+typedef struct s_ping
+{
+    char *ip_addr; // stock ip address 0.0.0.0 | 255.255.255.255 DNS lookup
+    char *reverse_hostname; // stock name domain  with associated a domain name Reverse DNS lookup
+    struct sockaddr_in serv_addr;
+
+
+
+} t_ping;
 
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include "../incl/ping.h"
 
-
 int main(int ac, char **av)
 {
     (void) av;
-    if (ac != 3)
+    if (ac != 2 && ac != 3)
+    {
+        ft_putstr_fd("sudo ./ft_ping [OPTION] <adresse>", 2);
         return (0);
+    }
     return (ac);
 }
